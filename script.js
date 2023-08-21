05af4b67cbdfd83383be97a52e40cd19e052c14a
ABHAY TARAS <abhaytaras18@gmail.com>
2023-08-21 09:55:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const emojiCount = 50;
-    const emojis = ['🎉', '😄', '🚀', '🌟', '❤️', '😊', '🎈', '🐱', '🍕', '🚲', '🍦', '🌺', '🌞', '🍓', '🎵', '🍭', '🍂', '🎓', '🏄‍♂️', '🎁', '🌍', '🐶', '🏆', '🍔', '🍀', '🎮', '🌹', '⚡️', '🍒', '📚', '🌄', '🚢', '🐰', '🎤', '🚁', '🍎', '🐢', '🎃', '🍒', '🌼', '🎊', '🌴', '🍁', '🍌', '🍂', '🌈', '🏀'];
+    let emojis = ['🎉', '😄', '🚀', '🌟', '❤️', '😊', '🎈', '🐱', '🍕', '🚲', '🍦', '🌺', '🌞', '🍓', '🎵', '🍭', '🍂', '🎓', '🏄‍♂️', '🎁', '🌍', '🐶', '🏆', '🍔', '🍀', '🎮', '🌹', '⚡️', '🍒', '📚', '🌄', '🚢', '🐰', '🎤', '🚁', '🍎', '🐢', '🎃', '🍒', '🌼', '🎊', '🌴', '🍁', '🍌', '🍂', '🌈', '🏀'];
+
+    const emojisContainer = document.querySelector('.emojis-container');
 
     for (let i = 0; i < emojiCount; i++) {
         const emoji = document.createElement('span');
@@ -8,6 +10,33 @@ document.addEventListener('DOMContentLoaded', function() {
         emoji.textContent = emojis[Math.floor(Math.random() * emojis.length)];
         emoji.style.left = `${Math.random() * 100}vw`;
         emoji.style.top = `${Math.random() * 100}vh`;
-        document.querySelector('.emojis-container').appendChild(emoji);
+        emojisContainer.appendChild(emoji);
+    }
+
+    emojisContainer.addEventListener('mousemove', function(event) {
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+
+        emojis = emojis.map(emoji => {
+            const distance = Math.sqrt(
+                Math.pow(mouseX - emojisContainer.offsetWidth / 2, 2) +
+                Math.pow(mouseY - emojisContainer.offsetHeight / 2, 2)
+            );
+
+            if (distance < 150) {
+                return '🌊'; // Change to wave emoji when cursor is near
+            } else {
+                return emoji;
+            }
+        });
+
+        updateEmojis();
+    });
+
+    function updateEmojis() {
+        const emojiElements = emojisContainer.querySelectorAll('.emoji');
+        emojiElements.forEach((emojiElement, index) => {
+            emojiElement.textContent = emojis[index];
+        });
     }
 });
