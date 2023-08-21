6ec70a11703fc5cde29615760ac8317e29d81b75
ABHAY TARAS <abhaytaras18@gmail.com>
2023-08-21 09:34:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const emojiCount = 50;
     const emojis = ['🎉', '😄', '🚀', '🌟', '❤️', '😊', '🎈', '🐱', '🍕', '🚲', '🍦', '🌺', '🌞', '🍓', '🎵', '🍭', '🍂', '🎓', '🏄‍♂️', '🎁', '🌍', '🐶', '🏆', '🍔', '🍀', '🎮', '🌹', '⚡️', '🍒', '📚', '🌄', '🚢', '🐰', '🎤', '🚁', '🍎', '🐢', '🎃', '🍒', '🌼', '🎊', '🌴', '🍁', '🍌', '🍂', '🌈', '🏀'];
 
-    const emojisContainer = document.querySelector('.emojis-container');
-
     for (let i = 0; i < emojiCount; i++) {
         const emoji = document.createElement('span');
         emoji.classList.add('emoji');
         emoji.textContent = emojis[Math.floor(Math.random() * emojis.length)];
         emoji.style.left = `${Math.random() * 100}vw`;
         emoji.style.top = `${Math.random() * 100}vh`;
-        emojisContainer.appendChild(emoji);
+        document.querySelector('.emojis-container').appendChild(emoji);
     }
 });
