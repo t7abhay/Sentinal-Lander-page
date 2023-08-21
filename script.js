cd29e7098b562be8b7cbc07c30f951d978470dc9
ABHAY TARAS <abhaytaras18@gmail.com>
2023-08-21 09:28:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
         emoji.classList.add('emoji');
         emoji.textContent = emojis[Math.floor(Math.random() * emojis.length)];
         emoji.style.left = `${Math.random() * 100}vw`;
-        emoji.style.animationDuration = `${Math.random() * 4 + 2}s`;
-        emoji.style.animationDelay = `${Math.random() * 2}s`;
+        emoji.style.top = `${Math.random() * 100}vh`;
         emojisContainer.appendChild(emoji);
     }
 });
